01a816f9d23043d8db4d15ba9750da3fe548336b
PikaaxYT <111752386+PikaaxYT@users.noreply.github.com>
2024-08-05 15:15:34 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,7 +1,1 @@
-echo "Installing NoVNC..."
-cd ~/
-git clone https://github.com/novnc/noVNC.git
-sudo apt-get install -q -y python3-pip
-pip3 install numpy
-cd noVNC
-openssl req -new -x509 -days 365 -nodes -out self.pem -keyout self.pem
+
