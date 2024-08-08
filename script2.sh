c723434f446d8ce71e63670d1ba8a59ee447bec9
PikaaxYT <111752386+PikaaxYT@users.noreply.github.com>
2024-08-08 13:57:58 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,14 +1,15 @@
-sudo apt install qemu-system
+echo "Installing ngrok..."
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update -q -y \
+	&& sudo apt install -q -y ngrok
+read -p "Paste your ngrok authtoken here:" authtoken
+ngrok config add-authtoken $authtoken
+echo "Installing QEMU..."
+sudo apt install -q -y qemu-system
 wget https://computernewb.com/isos/windows/Win8.1_English_x64.iso
 qemu-img create -f qcow2 win8.qcow2 30G
 qemu-system-x86_64 -enable-kvm -m 6G -smp 2 -hda win8.qcow2 -boot d -cdrom Win8.1_English_x64.iso -netdev user,id=net0,net=192.168.0.0/24,dhcpstart=192.168.0.9 -device rtl8139,netdev=net0 -vga qxl -device AC97 -vnc :1
--enable-kvm                                                    \
--m 4G                                                          \
--smp 2                                                         \
--hda myVirtualDisk.qcow2                                       \
--boot d                                                        \
--cdrom linuxmint-21.1-cinnamon-64bit.iso                       \
--netdev user,id=net0,net=192.168.0.0/24,dhcpstart=192.168.0.9  \
--device virtio-net-pci,netdev=net0                             \
--vga qxl                                                       \
--device AC97
+ngrok tcp 5901
