75b6558c80ec50ff91e7665b0a7e26620a2f9aae
PikaaxYT <111752386+PikaaxYT@users.noreply.github.com>
2024-08-10 15:07:58 +0200

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -19,5 +19,5 @@ echo "Creating virtual disk..."
 sudo qemu-img create -f qcow2 win8.qcow2 30G
 echo "Creating QEMU virtual machine..."
 chmod 777 /dev/kvm
-sudo qemu-system-x86_64 -enable-kvm -m 6G -smp 2 -hda win8.qcow2 -boot d -cdrom Win8.1_English_x64.iso -netdev user,id=net0,net=192.168.0.0/24,dhcpstart=192.168.0.9 -device rtl8139,netdev=net0 -vga qxl -device AC97 -vnc :1
+sudo qemu-system-x86_64 -m 6G -smp 2 -hda win8.qcow2 -boot d -cdrom Win8.1_English_x64.iso -netdev user,id=net0,net=192.168.0.0/24,dhcpstart=192.168.0.9 -device rtl8139,netdev=net0 -vga qxl -device AC97 -vnc :1
 ngrok tcp 5901
