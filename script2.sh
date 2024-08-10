cfa9cbca659919fdd411b3fe9faef8fd9f5043b7
PikaaxYT <111752386+PikaaxYT@users.noreply.github.com>
2024-08-10 14:08:38 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,8 +1,3 @@
-cd ~/
-sudo mkdir /mnt/sdb1
-sudo mount /dev/sdb1 /mnt/sdb1
-sudo chmod 777 /mnt/sdb1
-cd /mnt/sdb1
 echo "Installing ngrok..."
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
@@ -10,14 +5,14 @@ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	| sudo tee /etc/apt/sources.list.d/ngrok.list \
 	&& sudo apt update -q -y \
 	&& sudo apt install -q -y ngrok
-ngrok config add-authtoken 1tFgYaSEvTxLaqvJNHrJdyb8tQY_2N6EzW7tianaiHUZnpgoB
+read -p "Paste authtoken here:" $authtoken
+ngrok config add-authtoken authtoken
 echo "Installing QEMU..."
 sudo apt install -q -y qemu-system
 echo "Downloading Windows..."
 wget https://computernewb.com/isos/windows/Win8.1_English_x64.iso
 echo "Creating virtual disk..."
-sudo qemu-img create -f qcow2 win8.qcow2 30G
+sudo qemu-img create -f qcow2 win8.qcow2 40G
 echo "Creating QEMU virtual machine..."
-chmod 777 /dev/kvm
 sudo qemu-system-x86_64 -enable-kvm -m 6G -smp 2 -hda win8.qcow2 -boot d -cdrom Win8.1_English_x64.iso -netdev user,id=net0,net=192.168.0.0/24,dhcpstart=192.168.0.9 -device rtl8139,netdev=net0 -vga qxl -device AC97 -vnc :1
 ngrok tcp 5901
